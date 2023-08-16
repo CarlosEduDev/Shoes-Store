31aba22185d28db0615698176790871e692b6718
Carlos Eduardo Evangelista <CarlosEduardoEvang@gmail.com>
2023-08-16 18:01:00 -0300

    commit

---
+++ assets/JS/script.js
@@ -0,0 +1,7 @@
+//Animação de digitação
+// Typed.js
+
+const typed = new Typed('.animation', {
+   strings: ["O tênis Jordan é fruto de uma forte parceria entre Nike e o jogador Michael Jordan!"],
+   typeSpeed: 45,
+});
